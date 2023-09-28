fc9b7f2732ef7a9a7751143fae40747461160034
0xNordian <85548493+0xNordian@users.noreply.github.com>
2023-09-28 11:10:54 +0200

    menu fonts refactored

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -5,13 +5,13 @@ const NavMenu = () => {
     const authStore = useAuthStore()
 
     return (
-        <div className="nav-menu mt-4 flex items-center justify-center border-[1px] border-slate-700">
+        <div className="nav-menu mt-4 flex items-center justify-center border-b-[1px] border-slate-700">
             {authStore.isLoggedIn && (
                 <>
-                    <div className="grid h-10 w-1/2 place-items-center border-x-[1px]  border-slate-700 sm:w-1/4">
+                    <div className="grid h-10 w-1/2 place-items-center sm:w-1/4">
                         <NavLink to="/">Home</NavLink>
                     </div>
-                    <div className="grid h-10 w-1/2 place-items-center border-x-[1px] border-slate-700 sm:w-1/4">
+                    <div className="grid h-10 w-1/2 place-items-center sm:w-1/4">
                         <NavLink to="/starships">Starships</NavLink>
                     </div>
                 </>
